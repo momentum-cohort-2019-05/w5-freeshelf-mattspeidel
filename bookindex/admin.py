9503f531f37272ac6d3a9741697458b924b800e0
mattspeidel <mattspeidel@gmail.com>
2019-06-24 14:56:30 -0400

    add bookadmin

--- bookindex/admin.py
+++ bookindex/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bookindex.models import Category, Book
 
 # Register your models here.
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'description', 'date_added')
+
+    
