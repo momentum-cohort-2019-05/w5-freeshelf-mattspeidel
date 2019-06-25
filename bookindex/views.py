37daed39041e35d498b3885fcca1dc209c488f54
mattspeidel <mattspeidel@gmail.com>
2019-06-24 20:31:51 -0400

    add book_list.html, index page displays with basic initial layout

--- bookindex/views.py
+++ bookindex/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from bookindex.models import Book, Category
 
 # Create your views here.
-def index(request):
-    
-    return render(request, 'index.html',)
+class IndexListView(generic.ListView):
+    def index(self, request):
+        model = Book
+        return render(request, 'index.html',)
+
+    def get_queryset(self):
+        return Book.objects.order_by('date_added')
